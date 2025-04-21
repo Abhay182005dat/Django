47763715500ceebb359dd22d9e3b363f11071414
Abhay S N <abhay182005@gmail.com>
2025-04-21 17:10:55 +0530

    Lesson_3

---
+++ Lesson_3/myproject/posts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class post(models.Model):
+    title = models.CharField(max_length=75)
+    body = models.TextField()
+    slug = models.SlugField()
+    date = models.DateTimeField(auto_now_add=True) # everytime user adds a post data and time is added
