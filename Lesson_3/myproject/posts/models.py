c8c75a1ee1947cb41e3a2155983d4fec3ec92553
Abhay S N <abhay182005@gmail.com>
2025-04-26 14:43:26 +0530

    Lesson_4 in notes

--- Lesson_3/myproject/posts/models.py
+++ Lesson_3/myproject/posts/models.py
@@ -7,3 +7,8 @@ class post(models.Model):
     body = models.TextField()
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True) # everytime user adds a post data and time is added
+
+# Above line was till Lesson_3
+
+    def __str__(self):
+        return self.title     # Migrations is not necessary here beacuse we are not playing with data , its just a method
